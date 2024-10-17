38de92422f1b30833c61fb54f708c85021977efd
Giuseppe Calabrese <36233078+Giupest@users.noreply.github.com>
2024-10-17 06:52:51 +0000

    add passport authentication

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const { ObjectID } = require("mongodb");
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
@@ -31,6 +32,18 @@ app.use(express.urlencoded({ extended: true }));
 
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
+
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
 
   app.route("/").get((req, res) => {
     res.render("index", {
